92272a591548e4ad59d587d6ea0c502efff9ecc8
jerncomania28 <okonnduesojeremiah@gmail.com>
2022-11-15 07:49:00 +0000

    fix: text added and positioning of connect wallet corrected

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -3,15 +3,18 @@ import { ReactComponent as Forward } from "../assets/forward.svg";
 import WalletConnect from "../assets/wallet.svg";
 import { ReactComponent as Cancel } from "../assets/cancel.svg";
 
-const Wallet = ({handleShowWallet}) => {
+const Wallet = ({ handleShowWallet }) => {
   return (
-    <div className="min-w-[300px] w-[50%] max-w-[600px] rounded-lg shadow-sm bg-white absolute  top-[50%] left-[50%] transform -translate-x-[50%] translate-y-[40%] z-20 ">
+    <div className="min-w-[300px] w-[50%] max-w-[600px] rounded-lg shadow-sm bg-white absolute  top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[5%] z-20 ">
       <div className="flex justify-between items-center border-b-[1px] border-gray px-3 py-4">
         <p className=" text-[18px] md:text-[24px] text-[#333333] font-[700px]">
           Connect Wallet
         </p>
         <Cancel className="w-[20px]" onClick={handleShowWallet} />
       </div>
+      <p className="text-[14px] md:text-[16px] text-[#333333] w-[90%] mx-auto mt-2">
+        Choose your preferred wallet:
+      </p>
       <div className="flex justify-center items-center flex-col w-full relative py-6 px-2">
         <div className="py-2 w-[95%] mx-auto rounded shadow-lg flex justify-between items-center my-3">
           <div className="flex justify-center items-center mx-4">
